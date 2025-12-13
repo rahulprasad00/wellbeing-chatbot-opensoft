cd73cdc220afa7452c1f64a30c8eb1eab14361d7
Rahul Prasad <rahulrnc03@gmail.com>
2025-12-13 19:55:33 +0530

    Re optimised

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { Toaster } from "@/components/ui/sonner";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "HR-Employee AI Chatbot",
@@ -26,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <AuthProvider>{children}</AuthProvider>
         <Toaster position="top-right" />
       </body>
